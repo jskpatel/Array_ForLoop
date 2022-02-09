20bd27884dfc7acffa3cb7f1dbdb190230f6dbd6
dhavalp-hkis <86720731+dhavalp-hkis@users.noreply.github.com>
2022-02-09 10:26:15 +0530

    latest code

--- custom.js
+++ custom.js
@@ -1,6 +1,8 @@
 "use strict";
 
-let arrLength = prompt("Enter Array Length");
+// let arrLength = prompt("Enter Array Length");
+let arrLength = 15;
+console.log("Array Length -> ", arrLength);
 var arr = [];
 for (let i = 0; i < arrLength; i++) {
   arr[i] = i + 1;
@@ -10,18 +12,20 @@ console.log([...arr]);
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 // console.log(arr);
 
-let start = prompt("Enter start point");
-// console.log("start -> ", start);
+// let start = prompt("Enter start point");
+let start = 7;
+console.log("start -> ", start);
 
 let arrBeforeStart = arr.slice(0, start);
-// console.log(arrBeforeStart);
+console.log(arrBeforeStart);
 
 let arrAfterStart = arr.slice(start);
-// console.log(arrAfterStart);
+console.log(arrAfterStart);
 
-let frequency = prompt("Enter frequency point");
+// let frequency = prompt("Enter frequency point");
+let frequency = 2;
+console.log("frequency -> ", frequency);
 frequency++;
-// console.log("frequency -> ", frequency);
 
 var newArr = [];
 for (let i = 0; i < arrAfterStart.length; i++) {
@@ -30,7 +34,9 @@ for (let i = 0; i < arrAfterStart.length; i++) {
   }
   newArr[i] = arrAfterStart[i];
 }
-// console.log([...newArr]);
+
+console.log("Frequency added in array-> ", [...newArr]);
 
 arrBeforeStart.push(...newArr);
-console.log(arrBeforeStart);
+
+console.log("Final Array -> ", arrBeforeStart);
